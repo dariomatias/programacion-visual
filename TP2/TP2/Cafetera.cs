7d6ac448e6f618e1517511564d3cf77a35e1c58e
Dario Piriz <Dario.Piriz@qualitestgroup.com>
2021-08-16 22:41:39 -0300

    subi el ejercicio del TP2 (cafetera) con los metodos basicos mas una verificacion extra por disponible de cafe

---
+++ TP2/TP2/Cafetera.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace TP2
+{
+    class Cafetera
+    {
+        private const int taza = 100;
+        public int CapacidadMaxima { get; set; }
+        private static int CantidadActual { get; set; }
+
+        public Cafetera()
+        {
+            CapacidadMaxima = 1000;
+            CantidadActual = 0;
+        }
+
+        public void LlenarCafetera()
+        {
+            CantidadActual = CapacidadMaxima;
+            InformarCafeDisponible();
+        }
+
+        public void ServirTaza(int tazas)
+        {
+            if (CantidadActual < taza * tazas)
+                Console.WriteLine("No hay suficiente cafe.");
+            else
+            {
+                CantidadActual -= taza * tazas;
+                Console.WriteLine("Cafe(s) servido(s).");
+                InformarCafeDisponible();
+            }
+        }
+
+        public void InformarCafeDisponible()
+        {
+            Console.WriteLine("Hay " + CantidadActual + "cc de Cafe disponible para su consumo.");
+        }
+
+        public void VaciarCafetera()
+        {
+            CantidadActual = 0;
+            Console.WriteLine("Sin cafe. Por favor rellenar.");
+        }
+
+    }
+}
---
+++ TP2/TP2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cafetera c = new Cafetera();
+            c.InformarCafeDisponible(); //Debe ser 0cc
+            c.LlenarCafetera();
+
+            c.ServirTaza(10); //debe servir las 10 tazas
+            c.InformarCafeDisponible(); //debe ser 0cc 
+
+            c.ServirTaza(1);//debe arrojar error por falta de cafe
+            c.InformarCafeDisponible(); //0cc
+
+            c.LlenarCafetera();
+
+            c.VaciarCafetera();
+        }
+    }
+}

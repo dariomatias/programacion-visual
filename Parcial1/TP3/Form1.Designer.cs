da8dba35535a475a3db2e33f96bd4eaa9698f500
Dario Piriz <Dario.Piriz@qualitestgroup.com>
2021-10-07 23:39:36 -0300

    Actualice algunos metodos para validar el email y agregue las columnas a la grilla con la populacion de un nuevo registro.

---
+++ Parcial1/TP3/Form1.Designer.cs
@@ -0,0 +1,412 @@
+﻿
+namespace TP3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtDni = new System.Windows.Forms.TextBox();
+            this.txtLegajo = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.lblDni = new System.Windows.Forms.Label();
+            this.lblLegajo = new System.Windows.Forms.Label();
+            this.lblAnio = new System.Windows.Forms.Label();
+            this.txtAnio = new System.Windows.Forms.TextBox();
+            this.lblAlumnos = new System.Windows.Forms.Label();
+            this.lblNumAlumnos = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.lblCarrera = new System.Windows.Forms.Label();
+            this.txtCarrera = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblAnioNacimiento = new System.Windows.Forms.Label();
+            this.txtAnioDeNacimiento = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Apellido = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DNI = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.anio_nacimiento = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.legajo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.carrera = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.anio_inscripcion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(6, 58);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(389, 27);
+            this.txtNombre.TabIndex = 0;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(401, 58);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(388, 27);
+            this.txtApellido.TabIndex = 1;
+            // 
+            // txtDni
+            // 
+            this.txtDni.Location = new System.Drawing.Point(6, 112);
+            this.txtDni.Name = "txtDni";
+            this.txtDni.Size = new System.Drawing.Size(389, 27);
+            this.txtDni.TabIndex = 2;
+            // 
+            // txtLegajo
+            // 
+            this.txtLegajo.Location = new System.Drawing.Point(270, 45);
+            this.txtLegajo.Name = "txtLegajo";
+            this.txtLegajo.Size = new System.Drawing.Size(257, 27);
+            this.txtLegajo.TabIndex = 6;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(6, 35);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(64, 20);
+            this.lblNombre.TabIndex = 4;
+            this.lblNombre.Text = "Nombre";
+            this.lblNombre.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Location = new System.Drawing.Point(401, 35);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(66, 20);
+            this.lblApellido.TabIndex = 5;
+            this.lblApellido.Text = "Apellido";
+            this.lblApellido.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblDni
+            // 
+            this.lblDni.AutoSize = true;
+            this.lblDni.Location = new System.Drawing.Point(6, 89);
+            this.lblDni.Name = "lblDni";
+            this.lblDni.Size = new System.Drawing.Size(35, 20);
+            this.lblDni.TabIndex = 6;
+            this.lblDni.Text = "DNI";
+            this.lblDni.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblLegajo
+            // 
+            this.lblLegajo.AutoSize = true;
+            this.lblLegajo.Location = new System.Drawing.Point(270, 22);
+            this.lblLegajo.Name = "lblLegajo";
+            this.lblLegajo.Size = new System.Drawing.Size(54, 20);
+            this.lblLegajo.TabIndex = 7;
+            this.lblLegajo.Text = "Legajo";
+            this.lblLegajo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblAnio
+            // 
+            this.lblAnio.AutoSize = true;
+            this.lblAnio.Location = new System.Drawing.Point(533, 22);
+            this.lblAnio.Name = "lblAnio";
+            this.lblAnio.Size = new System.Drawing.Size(132, 20);
+            this.lblAnio.TabIndex = 9;
+            this.lblAnio.Text = "Año de Inscripción";
+            this.lblAnio.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtAnio
+            // 
+            this.txtAnio.Location = new System.Drawing.Point(533, 45);
+            this.txtAnio.Name = "txtAnio";
+            this.txtAnio.Size = new System.Drawing.Size(256, 27);
+            this.txtAnio.TabIndex = 7;
+            // 
+            // lblAlumnos
+            // 
+            this.lblAlumnos.AutoSize = true;
+            this.lblAlumnos.Location = new System.Drawing.Point(151, 9);
+            this.lblAlumnos.Name = "lblAlumnos";
+            this.lblAlumnos.Size = new System.Drawing.Size(135, 20);
+            this.lblAlumnos.TabIndex = 10;
+            this.lblAlumnos.Text = "Alumnos Anotados";
+            this.lblAlumnos.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblNumAlumnos
+            // 
+            this.lblNumAlumnos.AutoSize = true;
+            this.lblNumAlumnos.Location = new System.Drawing.Point(292, 9);
+            this.lblNumAlumnos.Name = "lblNumAlumnos";
+            this.lblNumAlumnos.Size = new System.Drawing.Size(36, 20);
+            this.lblNumAlumnos.TabIndex = 11;
+            this.lblNumAlumnos.Text = "N/A";
+            this.lblNumAlumnos.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(292, 363);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(94, 29);
+            this.btnGuardar.TabIndex = 8;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(411, 363);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(94, 29);
+            this.btnLimpiar.TabIndex = 9;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // lblCarrera
+            // 
+            this.lblCarrera.AutoSize = true;
+            this.lblCarrera.Location = new System.Drawing.Point(7, 22);
+            this.lblCarrera.Name = "lblCarrera";
+            this.lblCarrera.Size = new System.Drawing.Size(57, 20);
+            this.lblCarrera.TabIndex = 17;
+            this.lblCarrera.Text = "Carrera";
+            this.lblCarrera.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtCarrera
+            // 
+            this.txtCarrera.Location = new System.Drawing.Point(7, 45);
+            this.txtCarrera.Name = "txtCarrera";
+            this.txtCarrera.Size = new System.Drawing.Size(257, 27);
+            this.txtCarrera.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblEmail);
+            this.groupBox1.Controls.Add(this.txtEmail);
+            this.groupBox1.Controls.Add(this.lblAnioNacimiento);
+            this.groupBox1.Controls.Add(this.txtAnioDeNacimiento);
+            this.groupBox1.Controls.Add(this.lblNombre);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.lblApellido);
+            this.groupBox1.Controls.Add(this.txtApellido);
+            this.groupBox1.Controls.Add(this.lblDni);
+            this.groupBox1.Controls.Add(this.txtDni);
+            this.groupBox1.Location = new System.Drawing.Point(12, 32);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(795, 230);
+            this.groupBox1.TabIndex = 18;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos Personales";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(6, 156);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(129, 20);
+            this.lblEmail.TabIndex = 21;
+            this.lblEmail.Text = "Email de contacto";
+            this.lblEmail.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(6, 179);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(783, 27);
+            this.txtEmail.TabIndex = 4;
+            // 
+            // lblAnioNacimiento
+            // 
+            this.lblAnioNacimiento.AutoSize = true;
+            this.lblAnioNacimiento.Location = new System.Drawing.Point(401, 89);
+            this.lblAnioNacimiento.Name = "lblAnioNacimiento";
+            this.lblAnioNacimiento.Size = new System.Drawing.Size(138, 20);
+            this.lblAnioNacimiento.TabIndex = 19;
+            this.lblAnioNacimiento.Text = "Año de Nacimiento";
+            this.lblAnioNacimiento.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtAnioDeNacimiento
+            // 
+            this.txtAnioDeNacimiento.Location = new System.Drawing.Point(401, 112);
+            this.txtAnioDeNacimiento.Name = "txtAnioDeNacimiento";
+            this.txtAnioDeNacimiento.Size = new System.Drawing.Size(388, 27);
+            this.txtAnioDeNacimiento.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Nombre,
+            this.Apellido,
+            this.DNI,
+            this.anio_nacimiento,
+            this.Email,
+            this.legajo,
+            this.carrera,
+            this.anio_inscripcion});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 407);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(802, 289);
+            this.dataGridView1.TabIndex = 100;
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.MinimumWidth = 6;
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Width = 125;
+            // 
+            // Apellido
+            // 
+            this.Apellido.HeaderText = "Apellido";
+            this.Apellido.MinimumWidth = 6;
+            this.Apellido.Name = "Apellido";
+            this.Apellido.Width = 125;
+            // 
+            // DNI
+            // 
+            this.DNI.HeaderText = "DNI";
+            this.DNI.MinimumWidth = 6;
+            this.DNI.Name = "DNI";
+            this.DNI.Width = 125;
+            // 
+            // anio_nacimiento
+            // 
+            this.anio_nacimiento.HeaderText = "Año de Nacimiento";
+            this.anio_nacimiento.MinimumWidth = 6;
+            this.anio_nacimiento.Name = "anio_nacimiento";
+            this.anio_nacimiento.Width = 125;
+            // 
+            // Email
+            // 
+            this.Email.HeaderText = "Email de Contacto";
+            this.Email.MinimumWidth = 6;
+            this.Email.Name = "Email";
+            this.Email.Width = 125;
+            // 
+            // legajo
+            // 
+            this.legajo.HeaderText = "Legajo";
+            this.legajo.MinimumWidth = 6;
+            this.legajo.Name = "legajo";
+            this.legajo.Width = 125;
+            // 
+            // carrera
+            // 
+            this.carrera.HeaderText = "Carrera";
+            this.carrera.MinimumWidth = 6;
+            this.carrera.Name = "carrera";
+            this.carrera.Width = 125;
+            // 
+            // anio_inscripcion
+            // 
+            this.anio_inscripcion.HeaderText = "Año de Inscripción";
+            this.anio_inscripcion.MinimumWidth = 6;
+            this.anio_inscripcion.Name = "anio_inscripcion";
+            this.anio_inscripcion.Width = 125;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtAnio);
+            this.groupBox2.Controls.Add(this.lblLegajo);
+            this.groupBox2.Controls.Add(this.txtLegajo);
+            this.groupBox2.Controls.Add(this.lblAnio);
+            this.groupBox2.Controls.Add(this.lblCarrera);
+            this.groupBox2.Controls.Add(this.txtCarrera);
+            this.groupBox2.Location = new System.Drawing.Point(12, 268);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(795, 89);
+            this.groupBox2.TabIndex = 20;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Datos del Instituto";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(823, 708);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblNumAlumnos);
+            this.Controls.Add(this.lblAlumnos);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnGuardar);
+            this.Name = "Form1";
+            this.Text = "Primer Parcial";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtDni;
+        private System.Windows.Forms.TextBox txtLegajo;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.Label lblDni;
+        private System.Windows.Forms.Label lblLegajo;
+        private System.Windows.Forms.Label lblAnio;
+        private System.Windows.Forms.TextBox txtAnio;
+        private System.Windows.Forms.Label lblAlumnos;
+        private System.Windows.Forms.Label lblNumAlumnos;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Label lblCarrera;
+        private System.Windows.Forms.TextBox txtCarrera;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblAnioNacimiento;
+        private System.Windows.Forms.TextBox txtAnioDeNacimiento;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Apellido;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DNI;
+        private System.Windows.Forms.DataGridViewTextBoxColumn anio_nacimiento;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Email;
+        private System.Windows.Forms.DataGridViewTextBoxColumn legajo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn carrera;
+        private System.Windows.Forms.DataGridViewTextBoxColumn anio_inscripcion;
+    }
+}
+
---
+++ Parcial1/TP3/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace TP3
+{
+    public partial class Form1 : Form
+    {
+        private List<Alumno> alumnos = new();
+        private static List<TextBox> boxes = new();
+        private static Alumno alumnoEnCarga;
+
+        public Form1()
+        {
+            InitializeComponent();
+            boxes.Add(txtNombre);
+            boxes.Add(txtApellido);
+            boxes.Add(txtDni);
+            boxes.Add(txtAnioDeNacimiento);
+            boxes.Add(txtEmail);
+            boxes.Add(txtCarrera);
+            boxes.Add(txtLegajo);
+            boxes.Add(txtAnio);
+            ActualizarContador();
+        }
+        
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            LimpiarCampos();
+        }
+
+        public bool ValidarCampo(TextBox textBox)
+        {
+            if (textBox.Text.Trim() == "")
+            {
+                MessageBox.Show("Debe ingresar un dato válido en el campo " + textBox.Name.Substring(3));
+                textBox.Focus();
+                textBox.SelectAll();
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool ValidarForm()
+        {
+            foreach (TextBox box in boxes)
+            {
+                if (!ValidarCampo(box))
+                {
+                    return false;
+                }
+            }
+
+            try
+            {
+                txtDni.Focus();
+                var temp = Int32.Parse(txtDni.Text);
+
+                txtAnio.Focus();
+                temp = Int32.Parse(txtAnio.Text);
+                if (temp > DateTime.Now.Year)
+                    throw new Exception("El año de inscripción no puede ser en el futuro.");
+
+                txtAnioDeNacimiento.Focus();
+                temp = Int32.Parse(txtAnioDeNacimiento.Text);
+
+                if (DateTime.Now.Year - temp < 18)
+                    throw new Exception("El alumno debe ser mayor de 18 años.");
+
+                if (!ValidarEmail(txtEmail.Text.Trim()))
+                    throw new Exception("El email ingresado es inválido");
+
+
+            } catch (FormatException)
+            {
+                MessageBox.Show("Los datos numericos DNI, Año de Inscripción, o Año de Nacimiento son inválidos.");
+                return false;
+            } catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                return false;
+            }
+
+            return true;
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (ValidarForm())
+            {
+                alumnoEnCarga = new Alumno(txtNombre.Text, txtApellido.Text, Int32.Parse(txtDni.Text), txtLegajo.Text,
+                    txtCarrera.Text, Int32.Parse(txtAnio.Text), Int32.Parse(txtAnioDeNacimiento.Text),
+                    txtEmail.Text.Trim());
+                alumnos.Add(alumnoEnCarga);
+                LimpiarCampos();
+                ActualizarContador();
+                ActualizarGrilla();
+                txtNombre.Focus();
+            }
+        }
+
+        private void LimpiarCampos()
+        {
+            txtNombre.Text = "";
+            txtApellido.Text = "";
+            txtDni.Text = "";
+            txtAnioDeNacimiento.Text = "";
+            txtEmail.Text = "";
+            txtLegajo.Text = "";
+            txtCarrera.Text = "";
+            txtAnio.Text = "";
+            txtNombre.Focus();
+        }
+
+        private void ActualizarContador()
+        {
+            lblNumAlumnos.Text = alumnos.Count.ToString();
+        }
+
+        private void ActualizarGrilla()
+        {
+            dataGridView1.Rows.Add(alumnoEnCarga.Nombre, alumnoEnCarga.Apellido, alumnoEnCarga.Dni,
+                alumnoEnCarga.AnioDeNacimiento, alumnoEnCarga.Email, alumnoEnCarga.Legajo, alumnoEnCarga.Carrera,
+                alumnoEnCarga.AnioDeInscripcion);
+        }
+
+        bool ValidarEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}

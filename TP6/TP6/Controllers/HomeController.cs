f383ea2b489dbb93a6387ec0db25ac146a08ca8c
Dario Piriz <Dario.Piriz@qualitestgroup.com>
2021-11-18 20:52:26 -0300

    Updated TP6

---
+++ TP6/TP6/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TP6.Models;
+
+namespace TP6.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult Historia()
+        {
+            return View();
+        }
+        
+        public ActionResult Galeria()
+        {
+            return View();
+        }
+    }
+}
---
+++ TP6/TP6/Controllers/LibrosController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TP6.Models;
+
+namespace TP6.Controllers
+{
+    public class LibrosController : Controller
+    {
+        private LibreriaEntities db = new LibreriaEntities();
+
+        public ActionResult Index()
+        {
+            return View(db.Libros.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Libro libro = db.Libros.FirstOrDefault(l => l.Id == id);
+            if (libro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(libro);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Titulo,Autor,Paginas,ISBN,Edicion,Editorial,Lugar,FechaEdicion")] Libro libro)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Libros.Add(libro);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(libro);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Libro libro = db.Libros.Find(id);
+            if (libro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(libro);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Titulo,Autor,Paginas,ISBN,Edicion,Editorial,Lugar,FechaEdicion")] Libro libro)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(libro).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(libro);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Libro libro = db.Libros.Find(id);
+            if (libro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(libro);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Libro libro = db.Libros.Find(id);
+            db.Libros.Remove(libro);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
